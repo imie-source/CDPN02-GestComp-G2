56c88a07e2dad9f3c16922f2dc9e9188880c4d1e
Julien Chevet <chevetjulien@msn.com>
2013-11-28 11:49:51 +0100

    Add CompetenceDAO

--- Sources/src/dao/impl/CompetenceDAO.java
+++ Sources/src/dao/impl/CompetenceDAO.java
@@ -1,12 +1,13 @@
+package dao.impl;
 
-public class Main {
+public class CompetenceDAO {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
