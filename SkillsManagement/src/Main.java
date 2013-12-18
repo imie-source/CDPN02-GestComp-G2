64e7c45de4f18a1823aaa33e680c0552b936cd47
Julien Chevet <chevetjulien@msn.com>
2013-12-18 16:25:16 +0100

    Add Main class

---
+++ SkillsManagement/src/Main.java
@@ -0,0 +1,15 @@
+import dao.impl.CompetenceDAO;
+import dto.CompetenceDTO;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new CompetenceDTO();
+
+	}
+
+}
