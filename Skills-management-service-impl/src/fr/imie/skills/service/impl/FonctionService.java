dfae0aad4c4395e1cd6301b59de7ab9338e0144f
Eric Le Guellaut <eric.leguellaut@gmail.com>
2013-12-20 14:28:48 +0100

    Fonction Service: implémentation et interface

--- Skills-management-service-impl/src/fr/imie/skills/service/impl/FonctionService.java
+++ Skills-management-service-impl/src/fr/imie/skills/service/impl/FonctionService.java
@@ -1,13 +1,42 @@
 package fr.imie.skills.service.impl;
 
-public class FonctionService {
+import java.util.List;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import fr.imie.skills.dao.interfaces.IFonctionDAO;
+import fr.imie.skills.dao.interfaces.factory.AbstractDAOFactory;
+import fr.imie.skills.dto.FonctionDTO;
+import fr.imie.skills.service.interfaces.IFonctionService;
 
-	}
+public class FonctionService implements IFonctionService {
+
+
+private IFonctionDAO fonctionDAO = AbstractDAOFactory.getDAOFactory().getFonctionDAO();
+
+@Override
+public void updateFonction(FonctionDTO fonction) {
+	fonctionDAO.updateFonction(fonction);	
+}
+
+@Override
+public void insertFonction(FonctionDTO fonction) {
+	fonctionDAO.insertFonction(fonction);	
+}
+
+@Override
+public void deleteFonction(FonctionDTO fonction) {
+	fonctionDAO.deleteFonction(fonction);	
+}
+
+@Override
+public FonctionDTO getFonctionById(FonctionDTO fonction) {
+	FonctionDTO function = fonctionDAO.getFonctionById(fonction);
+	return function;
+}
+
+@Override
+public List<FonctionDTO> getAllFonction() {
+	List<FonctionDTO> functions = fonctionDAO.getAllFonction();
+	return functions;
+}
 
 }
--- Skills-management-service-int/src/fr/imie/skills/service/interfaces/IFonctionService.java
+++ Skills-management-service-int/src/fr/imie/skills/service/interfaces/IFonctionService.java
@@ -1,5 +1,18 @@
 package fr.imie.skills.service.interfaces;
 
+import java.util.List;
+
+import fr.imie.skills.dto.FonctionDTO;
+
 public interface IFonctionService {
 
+	void updateFonction(FonctionDTO fonction);
+
+	void insertFonction(FonctionDTO fonction);
+
+	void deleteFonction(FonctionDTO fonction);
+
+	FonctionDTO getFonctionById(FonctionDTO fonction);
+
+	List<FonctionDTO> getAllFonction();
 }
