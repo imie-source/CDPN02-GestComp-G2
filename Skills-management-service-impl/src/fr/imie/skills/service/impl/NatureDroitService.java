69e68502b09524debd5088d5955ce1777dec2fe0
Eric Le Guellaut <eric.leguellaut@gmail.com>
2013-12-20 15:00:14 +0100

    NatureDroitService: implémentation et interface

--- Skills-management-service-impl/src/fr/imie/skills/service/impl/NatureDroitService.java
+++ Skills-management-service-impl/src/fr/imie/skills/service/impl/NatureDroitService.java
@@ -1,13 +1,44 @@
 package fr.imie.skills.service.impl;
 
-public class NatureDroitService {
+import java.util.List;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import fr.imie.skills.dao.interfaces.INatureDroitDAO;
+import fr.imie.skills.dao.interfaces.factory.AbstractDAOFactory;
+import fr.imie.skills.dto.NatureDroitDTO;
+import fr.imie.skills.service.interfaces.INatureDroitService;
 
+public class NatureDroitService implements INatureDroitService{
+
+	private INatureDroitDAO natureDroitDAO = AbstractDAOFactory.getDAOFactory().getNatureDroitDAO();
+
+	@Override
+	public int deleteNatureDroit(NatureDroitDTO natureDroit) {
+		natureDroitDAO.deleteNatureDroit(natureDroit);
+		return 0;
+	}
+
+	@Override
+	public int insertNatureDroit(NatureDroitDTO natureDroit) {
+		natureDroitDAO.insertNatureDroit(natureDroit);
+		return 0;
+	}
+
+	@Override
+	public List<NatureDroitDTO> getAllNatureDroit() {
+		List<NatureDroitDTO> natureDroits = natureDroitDAO.getAllNatureDroit();
+		return natureDroits;
+	}
+
+	@Override
+	public NatureDroitDTO getNatureDroitByID(NatureDroitDTO natureDroit) {
+		NatureDroitDTO newNatureDroit = natureDroitDAO.getNatureDroitByID(natureDroit);
+		return newNatureDroit;
+	}
+
+	@Override
+	public int updateNatureDroit(NatureDroitDTO natureDroit) {
+		natureDroitDAO.updateNatureDroit(natureDroit);
+		return 0;
 	}
 
 }
