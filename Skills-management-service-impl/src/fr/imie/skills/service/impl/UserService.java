bdf3245090a01b6a7472e868fbff4a8fb99511d0
Guillaume Charpentreau <guillaume.charpentreau@hotmail.fr>
2013-12-20 13:57:17 +0100

    UserServicejava

--- Skills-management-service-impl/src/fr/imie/skills/service/impl/UserService.java
+++ Skills-management-service-impl/src/fr/imie/skills/service/impl/UserService.java
@@ -2,18 +2,27 @@ package fr.imie.skills.service.impl;
 
 import java.util.List;
 
-import dao.factory.FactoryDAO;
-import dao.interfaces.IUserDAO;
+import fr.imie.skills.dao.interfaces.factory.AbstractDAOFactory;
 import fr.imie.skills.dto.UserDTO;
 import fr.imie.skills.service.interfaces.IUserService;
 
 public class UserService implements IUserService {
+	@Override
+	public List<UserDTO> getAllUsers() {
+		List<UserDTO> usersList = AbstractDAOFactory.getDAOFactory().getUserDAO().getAllUser();
+		return usersList;
+	}
 
 	@Override
-	public List<UserDTO> listerUsers() {
-		new FactoryDAO();
-		IUserDAO userService = FactoryDAO.getUserDAO();
-		return null;
+	public int createPromotion(UserDTO user) {
+		int i =  AbstractDAOFactory.getDAOFactory().getUserDAO().insertUser(user);
+		return i;
+	}
+
+	@Override
+	public int deletePromotion(UserDTO user) {
+		int i =  AbstractDAOFactory.getDAOFactory().getUserDAO().insertUser(user);
+		return i;
 	}
 	
 }
--- Skills-management-service-int/src/fr/imie/skills/service/interfaces/IUserService.java
+++ Skills-management-service-int/src/fr/imie/skills/service/interfaces/IUserService.java
@@ -1,9 +1,12 @@
 package fr.imie.skills.service.interfaces;
 
 import java.util.List;
-
 import fr.imie.skills.dto.UserDTO;
 
 public interface IUserService {
-	List<UserDTO> listerUsers();
+	List<UserDTO> getAllUsers();
+	
+	int createPromotion(UserDTO user);
+	
+	int deletePromotion(UserDTO user);
 }
